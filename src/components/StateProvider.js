38274a6a29021f684e2ce5be1d39eef6d3120c08
karopeter <karopeter531@gmail.com>
2022-02-02 01:16:05 +0100

    new commit

--- src/components/StateProvider.js
+++ src/components/StateProvider.js
@@ -10,5 +10,7 @@ export const StateProvider = ({ reducer, initialState, children }) => (
     <StateContext.Provider value={useReducer(reducer, initialState)}>
         {children}
     </StateContext.Provider>
+);
 
-)
+// This is how we use it inside of a component 
+export const useStateValue = () => useContext(StateContext);
--- src/components/TransactionsHeader/TransactionsHeader.component.jsx
+++ src/components/TransactionsHeader/TransactionsHeader.component.jsx
@@ -3,15 +3,29 @@ import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Transactions from '../Transactions/Transactions.component';
+import { useStateValue } from '../StateProvider';
 import './TransactionsHeader.styles.scss';
 
-const TransactionsHeader = () => {
+  function TransactionsHeader({ id, title, layout,  recieved }) {
+   const [{ transaction }, dispatch] = useStateValue();
+   const addToTransaction = () => {
+     // Add item to transaction
+     dispatch({
+       type: 'ADD_TO_TRANSACTION',
+       item: {
+         id: id,
+         title: 'all',
+         layout: 'TRANSFERRED',
+         recieved:  'RECIEVED'
+       }
+     })
+   }
    return (
     <Container>
       <Row>
-        <Col xs=""><a href="#" className="item__layout">all</a></Col>
-        <Col xs={{ order: 12 }}><a href="#" className="item__layout">TRANSFERRED</a></Col>
-        <Col xs={{ order: 1 }}><a href="#" className="item__layout">RECIEVED</a></Col>
+        <Col xs=""><a className="item__layout">{title}</a></Col>
+        <Col xs={{ order: 12 }}><a className="item__layout">{layout}</a></Col>
+        <Col xs={{ order: 1 }}><a  className="item__layout">{recieved}</a></Col>
       </Row>
       <Transactions />
     </Container>
